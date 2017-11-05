f36dec80d6cca3e4d603ee9dde9f86076e8267c1
mrfearless <fearless.is.my.name@gmail.com>
2017-11-05 20:42:36 +0000

    Removed batch file

--- Masm64/go.bat
+++
@@ -1,7 +0,0 @@
-copy Masm64.lib m:\JWasm\lib\x64\Masm64.lib /y
-copy Masm64.inc m:\JWasm\include\Masm64.inc /y
-copy Masm64.lib m:\HJWasm\lib\x64\Masm64.lib /y
-copy Masm64.inc m:\HJWasm\include\Masm64.inc /y
-copy Masm64.lib m:\UASM\lib\x64\Masm64.lib /y
-copy Masm64.inc m:\UASM\include\Masm64.inc /y
-
