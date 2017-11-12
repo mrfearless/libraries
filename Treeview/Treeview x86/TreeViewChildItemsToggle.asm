f3ba2a246754d4e471e6403060e8712248415f0b
mrfearless <fearless.is.my.name@gmail.com>
2017-11-12 16:14:07 +0000

    Added TreeViewItemCollapse, TreeViewItemExpand and TreeViewItemToggle Properly implemented TreeViewChildItemsCollapse, TreeViewChildItemsExpand and TreeViewChildItemsToggle Updated radasm autocomplete files for new apis

--- Treeview/Treeview x86/TreeViewChildItemsToggle.asm
+++ Treeview/Treeview x86/TreeViewChildItemsToggle.asm
@@ -15,7 +15,20 @@ include TreeView.inc
 ; 
 ;**************************************************************************
 TreeViewChildItemsToggle PROC PUBLIC hTreeview:DWORD, hItem:DWORD
-    Invoke SendMessage, hTreeview, TVM_EXPAND, TVE_TOGGLE, hItem  
+    LOCAL hCurrentChild:DWORD
+    mov eax, hItem
+    mov hCurrentChild, eax
+    
+    Invoke SendMessage, hTreeview, WM_SETREDRAW, FALSE, 0
+    Invoke SendMessage, hTreeview, TVM_GETNEXTITEM, TVGN_CHILD, hCurrentChild
+    .WHILE eax != NULL
+        mov hCurrentChild, eax      
+        Invoke SendMessage, hTreeview, TVM_EXPAND, TVE_TOGGLE, hCurrentChild
+        Invoke SendMessage, hTreeview, TVM_GETNEXTITEM, TVGN_NEXT, hCurrentChild
+    .ENDW
+    Invoke SendMessage, hTreeview, TVM_GETNEXTITEM, TVGN_CARET, hItem
+    Invoke SendMessage, hTreeview, TVM_SELECTITEM, TVGN_FIRSTVISIBLE, hItem
+    Invoke SendMessage, hTreeview, WM_SETREDRAW, TRUE, 0    
     ret
 TreeViewChildItemsToggle endp
 
